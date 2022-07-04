88b3d699d06a61c943b4f8f7588a952b5f245f03
Sebastian Montes <107891851+s3b4st14nm@users.noreply.github.com>
2022-07-04 23:46:58 +0000

    setup completed

--- server.js
+++ server.js
@@ -9,10 +9,15 @@ const passport = require('passport');
 
 const app = express();
 
+
 fccTesting(app); //For FCC testing purposes
 app.set('view engine', 'pug');
 app.set("views", __dirname+ "/views/pug");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+console.log('Init: '+process.env.SESSION_SECRET);
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
@@ -20,9 +25,11 @@ app.use(session({
   cookie: { secure: false }
 }));
 
+app.use( passport.initialize() );
+app.use( passport.session() );
+
+
 app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
   res.render('index', {title: 'Hello', message: 'Please login'});
